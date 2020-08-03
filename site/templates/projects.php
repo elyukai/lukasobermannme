897353e04f01d7e748223ca287a70ad3a32c8c5e
Lukas Obermann <lukas.obermann@icloud.com>
2020-08-03 14:39:09 +0200

    Fixed debug config and skipped heading level

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -7,20 +7,20 @@
 </div>
 <section class="projects">
   <div class="wrapper">
-    <?php foreach ($page->children()->listed() as $project): ?>
-    <article>
-      <a href="<?= $project->url() ?>">
-        <?php $cover = $project->cover()->toFile() ?>
-        <img src="<?= $cover->url() ?>" alt="<?= $cover->alt() ?>">
-        <h3><?= $project->title()->html() ?></h3>
-        <ul>
-          <?php foreach ($project->scopes()->split() as $scope): ?>
-          <li><?= $scope ?></li>
-          <?php endforeach ?>
-        </ul>
-        <p class="read-more">Read More</p>
-      </a>
-    </article>
+    <?php foreach ($page->children()->listed() as $project) : ?>
+      <article>
+        <a href="<?= $project->url() ?>">
+          <?php $cover = $project->cover()->toFile() ?>
+          <img src="<?= $cover->url() ?>" alt="<?= $cover->alt() ?>">
+          <h2><?= $project->title()->html() ?></h2>
+          <ul>
+            <?php foreach ($project->scopes()->split() as $scope) : ?>
+              <li><?= $scope ?></li>
+            <?php endforeach ?>
+          </ul>
+          <p class="read-more">Read More</p>
+        </a>
+      </article>
     <?php endforeach ?>
   </div>
 </section>
