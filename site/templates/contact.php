05482cc4c7cdf5809cf4aadbb53b61aed3851d9b
Lukas Obermann <lukas.obermann@icloud.com>
2022-05-19 12:00:07 +0200

    feat: remove facebook

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -15,8 +15,10 @@
       <dd><a href="https://github.com/<?= $page->github()->html() ?>"><?= $page->github()->html() ?></a></dd>
       <dt>Twitter</dt>
       <dd><a href="https://twitter.com/<?= $page->twitter()->html() ?>">@<?= $page->twitter()->html() ?></a></dd>
+      <?php if ($page->facebook()->isNotEmpty()) : ?>
       <dt>Facebook</dt>
       <dd><a href="https://facebook.com/<?= $page->facebook()->html() ?>"><?= $page->facebook()->html() ?></a></dd>
+      <?php endif ?>
     </dl>
   </div>
 </section>
