f1612031597bdd857a03d54ca3144f5c9b30fcd5
Lukas Obermann <lukas.obermann@icloud.com>
2023-08-31 22:20:14 +0200

    feat: include all page translations in sitemap

--- site/snippets/sitemap.php
+++ site/snippets/sitemap.php
@@ -2,10 +2,14 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach ($pages as $p): ?>
         <?php if (in_array($p->uri(), $ignore)) continue ?>
+        <?php foreach(kirby()->languages() as $language): ?>
+        <?php if ($p->translation($language->code())->exists()) : ?>
         <url>
-            <loc><?= html($p->url()) ?></loc>
+            <loc><?= html($p->url($language->code())) ?></loc>
             <lastmod><?= $p->modified('c', 'date') ?></lastmod>
             <priority><?= ($p->isHomePage()) ? 1 : number_format(0.5 / $p->depth(), 1) ?></priority>
         </url>
+        <?php endif ?>
+        <?php endforeach ?>
     <?php endforeach ?>
 </urlset>
