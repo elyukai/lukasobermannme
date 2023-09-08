4851ea988a6518c4d4e2068b52b2c4eae4d686d6
Lukas Obermann <lukas.obermann@icloud.com>
2023-09-08 21:47:01 +0200

    fix: typewriter cursor inconsistent positioning in firefox

--- assets/js/simple-typewriter.js
+++ assets/js/simple-typewriter.js
@@ -7,14 +7,33 @@
 
   const SPAN_SHOWN_CLASS_NAME = "typewriter-text--shown"
   const SPAN_HIDDEN_CLASS_NAME = "typewriter-text--hidden"
-  const SPAN_CURSOR_CLASS_NAME = "typewriter-text--cursor"
+  const SPAN_CURSOR_CLASS_NAME = "typewriter-cursor"
 
   const elements = /** @type {NodeListOf<HTMLElement>}*/ (document.querySelectorAll(`.${TRIGGER_CLASS_NAME}`))
 
   /**
+   * @param {number} value
+   * @param {number} min
+   * @param {number} max
+   * @returns {boolean}
+   */
+  const isInRange = (value, min, max) => value >= min && value <= max
+
+  /**
    * @param {HTMLElement} element
    */
-  const isElementInView = element => element.getBoundingClientRect().top < window.innerHeight
+  const isElementScrolledBy = element => {
+    const elementBounds = element.getBoundingClientRect()
+    return element.getBoundingClientRect().bottom <= 0
+  }
+
+  /**
+   * @param {HTMLElement} element
+   */
+  const isElementInView = element => {
+    const elementBounds = element.getBoundingClientRect()
+    return elementBounds.bottom > 0 && elementBounds.top < window.innerHeight
+  }
 
   /**
    * @param {number} ms
@@ -44,27 +63,16 @@
 
     const textParts = extractTextNodes(element)
 
-    let cursorState = true
-
-    /**
-     * @type {undefined | ((state: boolean) => void)}
-     */
-    let listener = undefined
-
     const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)")
     let reducedMotion = mediaQuery.matches
     mediaQuery.addEventListener("change", event => {
       reducedMotion = event.matches
     })
 
-    const timer = setInterval(() => {
-      if (listener) {
-        listener(reducedMotion || cursorState)
-      }
-      cursorState = !cursorState
-    }, 500)
-
     element.classList.add(ACTIVE_CLASS_NAME)
+
+    const cursorElement = document.createElement("span")
+    cursorElement.classList.add(SPAN_CURSOR_CLASS_NAME)
 
     const replacedTextParts = textParts.map(textPart => {
       const initialText = textPart.textContent ?? ""
@@ -84,48 +92,37 @@
     })
 
     for (const { initialText, textPart, showTextElement, hiddenTextElement } of replacedTextParts) {
-      listener = state => {
-        if (state) {
-          showTextElement.classList.add(SPAN_CURSOR_CLASS_NAME)
-        }
-        else {
-          showTextElement.classList.remove(SPAN_CURSOR_CLASS_NAME)
-        }
-      }
-
-      listener(cursorState)
+      /** @type {ParentNode} */ (hiddenTextElement.parentNode).insertBefore(cursorElement, hiddenTextElement)
 
       for (let i = 0; i < initialText.length; i++) {
         showTextElement.textContent = initialText.slice(0, i + 1)
         hiddenTextElement.textContent = initialText.slice(i + 1)
 
-        await delay(Math.random() * 30 + 15)
+        await delay(Math.random() * 40 + 10)
       }
 
       if (textPart !== textParts[textParts.length - 1] || !element.classList.contains(KEEP_CURSOR_CLASS_NAME)) {
-        listener = undefined
         showTextElement.replaceWith(textPart)
+        cursorElement.remove()
         hiddenTextElement.remove()
       }
-    }
-
-    if (!element.classList.contains(KEEP_CURSOR_CLASS_NAME)) {
-      clearInterval(timer)
     }
   }
 
   elements.forEach(element => {
-    if (isElementInView(element)) {
+    if (isElementScrolledBy(element)) {
+      element.classList.add(ACTIVE_CLASS_NAME)
+    } else if (isElementInView(element)) {
       typewrite(element)
-    }
-    else {
-      let isTriggered = false
-      window.addEventListener("scroll", () => {
-        if (!isTriggered && isElementInView(element)) {
+    } else {
+      const listener = () => {
+        if (isElementInView(element)) {
           typewrite(element)
-          isTriggered = true
+          window.removeEventListener("scroll", listener)
         }
-      })
+      }
+
+      window.addEventListener("scroll", listener)
     }
   })
 })()
