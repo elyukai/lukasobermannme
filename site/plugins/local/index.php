d326b0a8f11aca880d256485275468466834ead8
Lukas Obermann <lukas.obermann@icloud.com>
2023-09-09 10:09:49 +0200

    feat: fingerprinting for css and js files

--- site/plugins/local/index.php
+++ site/plugins/local/index.php
@@ -1,3 +1,33 @@
 <?php
 
 require_once __DIR__ . '/D.php';
+
+use Kirby\Cms\App;
+use Kirby\Cms\App as Kirby;
+use Kirby\Filesystem\F;
+use Kirby\Http\Url;
+
+Kirby::plugin('lukasobermann/local', [
+   'components' => [
+      'css' => function (App $kirby, string $url, $options = null): string {
+          $relative_url = Url::path($url, false);
+          $file_root = $kirby->root('index') . '/' . $relative_url;
+
+          if (F::exists($file_root)) {
+              return url($relative_url . '?' . F::modified($file_root));
+          }
+
+          return $url;
+      },
+      'js' => function (App $kirby, string $url, $options = null): string {
+          $relative_url = Url::path($url, false);
+          $file_root = $kirby->root('index') . '/' . $relative_url;
+
+          if (F::exists($file_root)) {
+              return url($relative_url . '?' . F::modified($file_root));
+          }
+
+          return $url;
+      }
+  ]
+]);
