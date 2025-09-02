60f3faf1fa1ed4ab94928212eb6961f60290e9df
Lukas Obermann <lukas.obermann@icloud.com>
2025-09-02 10:58:49 +0200

    feat: center layout if viewport wider than 120 rem

--- site/snippets/layout.php
+++ site/snippets/layout.php
@@ -36,46 +36,48 @@
 </head>
 
 <body class="<?= $page->template()->name() ?>">
-  <a href="#content" class="skip-link">Skip to main content</a>
-  <header>
-    <nav class="main">
-      <h2 class="sr-only"><?= t('Primary Navigation') ?></h2>
-      <ul>
-        <?php $headerMenu = $site->headerMenu()->toPages(); ?>
-        <?php foreach ($headerMenu as $headerMenuItem) : ?>
-          <li>
-            <a href="<?= $headerMenuItem->url() ?>"<?= ($headerMenuItem->id() === $page->id() || $headerMenuItem->isAncestorOf($page)) ? ' aria-current="page"' : '' ?>><?= $headerMenuItem->title()->html() ?></a>
+  <div class="max-width">
+    <a href="#content" class="skip-link">Skip to main content</a>
+    <header>
+      <nav class="main">
+        <h2 class="sr-only"><?= t('Primary Navigation') ?></h2>
+        <ul>
+          <?php $headerMenu = $site->headerMenu()->toPages(); ?>
+          <?php foreach ($headerMenu as $headerMenuItem) : ?>
+            <li>
+              <a href="<?= $headerMenuItem->url() ?>"<?= ($headerMenuItem->id() === $page->id() || $headerMenuItem->isAncestorOf($page)) ? ' aria-current="page"' : '' ?>><?= $headerMenuItem->title()->html() ?></a>
+            </li>
+          <?php endforeach ?>
+        </ul>
+      </nav>
+      <nav class="languages">
+        <h2 class="sr-only"><?= t('Language Menu') ?></h2>
+        <ul>
+          <?php foreach($kirby->languages() as $language): ?>
+          <li<?php e($kirby->language() == $language, ' class="active"') ?>>
+            <a href="<?= $page->url($language->code()) ?>" hreflang="<?php echo $language->code() ?>"><?= html($language->name()) ?></a>
           </li>
-        <?php endforeach ?>
-      </ul>
-    </nav>
-    <nav class="languages">
-      <h2 class="sr-only"><?= t('Language Menu') ?></h2>
-      <ul>
-        <?php foreach($kirby->languages() as $language): ?>
-        <li<?php e($kirby->language() == $language, ' class="active"') ?>>
-          <a href="<?= $page->url($language->code()) ?>" hreflang="<?php echo $language->code() ?>"><?= html($language->name()) ?></a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-    </nav>
-  </header>
-  <main id="content">
-    <?= $slots->default() ?>
-  </main>
-  <footer class="aos">
-    <?php $footerMenu = $site->footerMenu()->toPages(); ?>
-    <?php if ($footerMenu->isNotEmpty()) : ?>
-    <nav class="footer">
-      <ul>
-        <?php foreach ($footerMenu as $footerMenuItem): ?>
-        <li><a href="<?= $footerMenuItem->url() ?>"<?= ($footerMenuItem->id() === $page->id() || $footerMenuItem->isAncestorOf($page)) ? ' aria-current="page"' : '' ?>><?= $footerMenuItem->title()->html() ?></a></li>
-        <?php endforeach ?>
-      </ul>
-    </nav>
-    <?php endif ?>
-    <p class="copyright"><?= $site->copyright()->kti() ?></p>
-  </footer>
+          <?php endforeach ?>
+        </ul>
+      </nav>
+    </header>
+    <main id="content">
+      <?= $slots->default() ?>
+    </main>
+    <footer class="aos">
+      <?php $footerMenu = $site->footerMenu()->toPages(); ?>
+      <?php if ($footerMenu->isNotEmpty()) : ?>
+      <nav class="footer">
+        <ul>
+          <?php foreach ($footerMenu as $footerMenuItem): ?>
+          <li><a href="<?= $footerMenuItem->url() ?>"<?= ($footerMenuItem->id() === $page->id() || $footerMenuItem->isAncestorOf($page)) ? ' aria-current="page"' : '' ?>><?= $footerMenuItem->title()->html() ?></a></li>
+          <?php endforeach ?>
+        </ul>
+      </nav>
+      <?php endif ?>
+      <p class="copyright"><?= $site->copyright()->kti() ?></p>
+    </footer>
+  </div>
 
   <?= js([
     'assets/js/enabled.js',
